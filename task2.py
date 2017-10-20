868de2d19b5042d0ce9d594b3e0daef45ba93d75
singhbikramjit <bikramjit.singh@dal.ca>
2017-10-20 10:59:08 -0700

    task2

--- task2.py
+++ task2.py
@@ -1,7 +1,8 @@
-#from flask import flask
+
 from weather import Weather
+#variables assign
 weather = Weather
-cityname=input("enter city")
+cityname=input("enter city") #input data from user in text form
 location = weather.lookup_by_location(cityname)
 condition = location.condition()
 
@@ -13,4 +14,15 @@ ltemp=[] #empty list for low temprature
 #output data for inputed day
 print("current weather is ", condition['text'])
 print("current temperature ",condition['temp'])
+#data for next days 
 
+print("data for next 5 days are :")
+
+for forecasts in location.forecast():
+        htemp = forecasts['high']  #fetching high temp
+        ltemp = forecasts['low'] #fetching low temp
+        rain= forecast['text'] #data  for rain 
+        if forecasts['text']== "Rain":
+                print("rain will be on ",forecasts['day'])
+
+#end of prog  task2 --bikramjit singh 
